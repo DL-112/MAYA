ff1ef91cd0b69773b43f13e08e13bb1c256ac246
Wai Yan Htut <waiyanhtut354@gmail.com>
2024-11-30 19:59:00 +0630

    added word breaker

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import tkinter as tk
 from tkinter import scrolledtext
 import train_bigram as tb
+import wordbreak as wb
 
-def process_text():
+def bigram_predict():
     file_path = "shwe_u_daung.txt"
     probabilities = tb.train_bigram(file_path)
     output_text = tb.generate_text(probabilities, "ကြွယ်", 50)
+    output_text_area.delete("1.0", tk.END)  # Clear previous output
+    output_text_area.insert(tk.END, output_text)  # Insert new output
+
+def word_break():
+    input_string = "သဘာဝဟာသဘာဝပါ"
+    output_text = wb.wordbreak(input_string)
     output_text_area.delete("1.0", tk.END)  # Clear previous output
     output_text_area.insert(tk.END, output_text)  # Insert new output
 
@@ -14,9 +21,13 @@ root = tk.Tk()
 root.title("MAYA")
 root.geometry("400x300")
 
-# Create process button
-process_button = tk.Button(root, text="Process", command=process_text)
-process_button.pack()
+# Create button for bigram prediction
+bigram_btn = tk.Button(root, text="Bigram", command=bigram_predict)
+bigram_btn.pack()
+
+# Create button for word breaker
+wordbreaker_btn = tk.Button(root, text="Word break", command=word_break)
+wordbreaker_btn.pack()
 
 # Create output text area
 output_label = tk.Label(root, text="Output:")
